c1738e24b373430f9630cce749a614e8a81f7841
Abraham <43353411+Rezatachi@users.noreply.github.com>
2021-03-15 17:38:23 -0400

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,7 @@ const Nav = ({ libraryStatus, setlibraryStatus }) => {
       <h1>Fidelity</h1>
       <div className="btn">
         <CleanButton
-          style={{ outline: "none", border: "none" }}
+          style={{ outline: "none", border: "none", cursor: "pointer" }}
           isPrimary
           onClick={() => setlibraryStatus(!libraryStatus)}
         >
